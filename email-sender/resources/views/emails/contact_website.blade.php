63d8cfaa96a9e44df0cbf6e90c12471af57ada97
Assovio <rodrigo@assovio.com.br>
2020-07-01 20:09:25 -0300

    Fix/Ajustado o template do email contato site

--- email-sender/resources/views/emails/contact_website.blade.php
+++ email-sender/resources/views/emails/contact_website.blade.php
@@ -6,7 +6,7 @@
 	<p>Assunto: {{$contact->subject}}</p>
 	<p>Mensagem: {{$contact->message}}</p>
 	
-	<p>Mensagem: {{$contact->client->name}}</p>
-	<p>Mensagem: {{$contact->client->urlSite}}</p>
-	<p>Mensagem: {{$contact->client->urlLogo}}</p>
+	<p>Nome: {{$contact->client->name}}</p>
+	<p>Site: {{$contact->client->urlSite}}</p>
+	<p>Logo: {{$contact->client->urlLogo}}</p>
 </div>
