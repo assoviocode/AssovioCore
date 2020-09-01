290b73a5b086085c32095eed6401848881c76352
Assovio <rodrigo@assovio.com.br>
2020-09-01 17:09:11 -0300

    fix: Incluido layout email sobrinho advogados

--- email-sender/app/Http/Controllers/ForgotPasswordController.php
+++ email-sender/app/Http/Controllers/ForgotPasswordController.php
@@ -1,7 +1,8 @@
 <?php
 namespace app\Http\Controllers;
 
-use App\Mail\CBrancoAdv\MailForgotPassword;
+use App\Client;
+use App\Mail\EmailForgotPassword;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Laravel\Lumen\Routing\Controller as BaseController;
@@ -12,13 +13,21 @@ class ForgotPasswordController extends BaseController
     public function send(Request $request)
     {
         $this->validate($request, [
-            'client_name' => 'required|max:255',
+            'name' => 'required|max:255',
             'email' => 'required|email',
-            'link_button' => 'required',
+            'link' => 'required',
             'key' => 'required'
         ]);
 
-        Mail::to($request->email)->send(new MailForgotPassword($request->link_button, $request->client_name));
+        $client = Client::where('key', $request->key)->first();
+
+        if (is_null($client)) {
+            return response()->json([
+                'message' => 'Bad request'
+            ], 401);
+        }
+
+        Mail::to($request->email)->send(new EmailForgotPassword($request->link, $request->name, $client));
 
         return response()->json([
             'message' => 'Email enviado com sucesso!'
--- email-sender/resources/views/emails/adv_sobrinho/register_client.blade.php
+++ email-sender/resources/views/emails/adv_sobrinho/register_client.blade.php
@@ -1,16 +1,20 @@
 @extends('emails.adv_sobrinho.template')
 
 @section('banner')
-	
+	<img align="center" title="Grupo Sobrinho Advogados" alt="Capa" src="http://development.gruposobrinhoadvogados.kinghost.net/front-sistema/email/img/default.jpg" width="564" style="max-width:564px; padding-bottom: 0; display: inline !important; vertical-align: bottom; margin-bottom: 10px;" class="mcnImage">
 @stop
 
 @section('content')
-	<p>Url do site: {{$client->getUrlSite()}}</p>
-    <p>Email: Novo Registro</p>
-    <p>Cliente: {{$consumerName}}</p>
-    <p>Link: {{$linkButton}}</p>
+	<tr>
+      <td valign="top" class="mcnTextContent" style="padding-top:0; padding-right:18px; padding-bottom:9px; padding-left:18px;">
+        <h3>BEM VINDO!</h3>
+        <p>Olá {{$consumerName}}, agradecemos por utilizar nossos serviços. Clique no botão abaixo e confirme seu cadastro.</p>
+      </td>
+    </tr>
 @stop
 
 @section('button')
-	
+    <td align="center" valign="middle" class="mcnButtonContent" style="font-family: Verdana, Geneva, sans-serif; font-size: 14px; padding: 15px 30px;">
+    	<a class="mcnButton " title="Site" href={{$linkButton}} target="_blank" style="font-weight: bold;letter-spacing: normal;line-height: 100%;text-align: center;text-decoration: none;color: #FFFFFF;">Confirmar Cadastro</a>
+    </td>
 @stop
